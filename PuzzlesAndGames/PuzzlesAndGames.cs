78f8f2abb530de9e5e5d170fd2f2f2af008aea84
Amine Ghilazghi <amineteclai@gmail.com>
2017-10-29 13:42:27 -0400

    Multiple of 11 or  one more than multiple of 11

--- PuzzlesAndGames/PuzzlesAndGames.cs
+++ PuzzlesAndGames/PuzzlesAndGames.cs
@@ -13,13 +13,32 @@ namespace PuzzlesAndGames
             Assert.AreEqual(true, actual);
         }
 
+        [TestMethod]
+        public void Test_IsCool()
+        {
+            var actual = IsCool(22);
+            Assert.AreEqual(true, actual);
+        }
 
+        /*A number is cool if it's a multiple of 11 or if is one more than 
+         * a multiple of 11. Return true if the given number is cool.
+         */
+        public bool IsCool(int givenNumber)
+        {
+            if(givenNumber < 1)
+            {
+                throw new ArgumentException();
+            }
+
+            return (givenNumber % 11 == 0 || givenNumber % 11 == 1);
+        }
+            
         /*
-         * Given three integers, first, second, and third return if second is greater 
-         * than first, and third is greater than second. However, with the exception 
-         * that if the parameter "isOk" is true, second does not need to be greater than 
+         * Given three integers, if second is greater than first, and third is 
+         * greater than second. However, with the exception that if the 
+         * parameter "isOk" is true, second does not need to be greater than 
          * first but still better be less than third.
          */
-        public bool IsOrdered(int first, int second, int third, bool isOk) => isOk ? (first < third && second < third) : first < second && second < third;
+        public bool IsOrdered(int first, int second, int third, bool isOk) => isOk ? (first < third && second < third) : (first < second && second < third);
     }
 }
