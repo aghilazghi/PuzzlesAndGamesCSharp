7e0c52f844ff9599f353c84e7924696477b00118
Amine Ghilazghi <amineteclai@gmail.com>
2017-10-29 13:02:15 -0400

    Check the order of three numbers

---
+++ PuzzlesAndGames/PuzzlesAndGames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PuzzlesAndGames
+{
+    [TestClass]
+    public class PuzzlesAndGames
+    {
+        [TestMethod]
+        public void Test_IsOrdered()
+        {
+            var actual = IsOrdered(1, 3, 4, false);
+            Assert.AreEqual(true, actual);
+        }
+
+
+        /*
+         * Given three integers, first, second, and third return if second is greater 
+         * than first, and third is greater than second. However, with the exception 
+         * that if the parameter "isOk" is true, second does not need to be greater than 
+         * first but still better be less than third.
+         */
+        public bool IsOrdered(int first, int second, int third, bool isOk) => isOk ? (first < third && second < third) : first < second && second < third;
+    }
+}
