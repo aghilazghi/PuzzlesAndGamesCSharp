86ea9e219fec7c7a66e869b5b8521b004c5d0586
Amine Ghilazghi <amineteclai@gmail.com>
2017-11-01 13:54:39 -0400

    Returns nearest to 21 without going over

---
+++ PuzzlesAndGames.Test/NearestToTwentyOneTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesAndGamesLibrary;
+
+namespace PuzzlesAndGames.Test
+{
+    [TestClass]
+    public class NearestToTwentyOneTest
+    {
+        [TestMethod]
+        public void Test_Near_InputFirstLtSecondLtEq21_ExpectedSecondReturnsSecond()
+        {
+            var tewnetyOne = new NearestToTwentyOne();
+            var expected = 21;
+            var actual = tewnetyOne.Near(18, 21);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Near_InputSecondLtFirstLtEq21_ExpectedFirstReturnsFirst()
+        {
+            var tewnetyOne = new NearestToTwentyOne();
+            var expected = 21;
+            var actual = tewnetyOne.Near(21, 17);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Near_InputFirstLt21SecondGt21_ExpectedFirstReturnsFirst()
+        {
+            var tewnetyOne = new NearestToTwentyOne();
+            var expected = 16;
+            var actual = tewnetyOne.Near(16, 22);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Near_InputSecondLt21FirstGt21_ExpectedSecondReturnsSecond()
+        {
+            var tewnetyOne = new NearestToTwentyOne();
+            var expected = 17;
+            var actual = tewnetyOne.Near(22, 17);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Near_InputSecondAndFirstGt21_ExpectedZeroReturnsZero()
+        {
+            var tewnetyOne = new NearestToTwentyOne();
+            var expected = 0;
+            var actual = tewnetyOne.Near(22, 50);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PuzzlesAndGamesLibrary/NearestToTwentyOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzlesAndGamesLibrary
+{
+    public class NearestToTwentyOne
+    {
+        /* Given two positive integers arguments, return whichever is nearest
+         * to 21 without going over. Return 0 if they go over 21.
+         */
+        public int Near(int a, int b)
+        {
+            if ((a >= b && a <= 21) || (b > 21 && a <= 21)) return a;
+            if ((b >= a && b <= 21) || (a > 21 && b <= 21)) return b;
+
+            return 0;
+        }
+        
+    }
+}
