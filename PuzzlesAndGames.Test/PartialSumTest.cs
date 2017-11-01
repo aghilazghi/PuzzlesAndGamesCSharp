f84c8051416006fc78af536858201c187752a567
Amine Ghilazghi <amineteclai@gmail.com>
2017-11-01 14:27:09 -0400

    Return partial sum

---
+++ PuzzlesAndGames.Test/PartialSumTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzlesAndGamesLibrary;
+
+namespace PuzzlesAndGames.Test
+{
+    [TestClass]
+    public class PartialSumTest
+    {
+        [TestMethod]
+        public void Test_Sum_FirstParamEq13_ExpectedZeroReturnsZero()
+        {
+            var partial = new PartialSum();
+            var expected = 0;
+            var actual = partial.Sum(13, 2, 3);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Sum_SecondParamEq13_ExpectedFirstParamReturnsFirstParam()
+        {
+            var partial = new PartialSum();
+            var expected = 9;
+            var actual = partial.Sum(9, 13, 3);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Sum_ThirdParamEq13_ExpectedSumOfFirstAndSecondParamsRseturnsSumOfFirstAndSecondParams()
+        {
+            var partial = new PartialSum();
+            var expected = 23;
+            var actual = partial.Sum(9, 14, 13);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Sum_AllParamsNotEq13_ExpectedSumOfAllParamsRseturnsSumOfAllParams()
+        {
+            var partial = new PartialSum();
+            var expected = 23;
+            var actual = partial.Sum(9, 14, 13);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PuzzlesAndGamesLibrary/PartialSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzlesAndGamesLibrary
+{
+    public class PartialSum
+    {
+        /*
+         * Given 3 integer arguments a, b, and c return their sum. However, if one
+         * of the arguments is 13 then it does not count towards the sum and 
+         * arguments to its right do not count either.
+         */
+
+        public int Sum(int a, int b, int c)
+        {
+            if (a == 13) return 0;
+            if (b == 13) return a;
+            if (c == 13) return a + b;
+
+            return a + b + c;
+        }
+    }
+}
